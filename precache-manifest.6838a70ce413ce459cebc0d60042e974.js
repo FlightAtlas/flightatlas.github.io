c964004605d4fe180b9f968832a4b5108fe6a553
Nima Rahnemoon <nrahnemoon@gmail.com>
2024-04-01 13:56:40 -0700

    Updates

---
+++ precache-manifest.6838a70ce413ce459cebc0d60042e974.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de06d576c3ae991138d46fedbd33a2fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9086b19ef4591bd37221",
+    "url": "/static/css/main.d8521a99.chunk.css"
+  },
+  {
+    "revision": "28c38b3491202f5fe6f4",
+    "url": "/static/js/2.b03c2a73.chunk.js"
+  },
+  {
+    "revision": "9086b19ef4591bd37221",
+    "url": "/static/js/main.3726c935.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
